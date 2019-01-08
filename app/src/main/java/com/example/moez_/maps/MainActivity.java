4f0c34c1d16a102ee645a58891e2a37ea9ec1ef9
moez-baksi <moez-baksi@hotmail.com>
2019-01-08 16:00:40 +0100

    version 0.2

--- app/src/main/java/com/example/moez_/maps/MainActivity.java
+++ app/src/main/java/com/example/moez_/maps/MainActivity.java
@@ -1,14 +1,26 @@
 package com.example.moez_.maps;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
-    // todo
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void starting (View view) {
+        // Let the start stop and return
+        Intent intent = new Intent(MainActivity.this, MapsActivity.class);
+        startActivity(intent);
+    }
+
+    public void scoring(View view) {
+        // Let the start stop and return
+        Intent intent = new Intent(MainActivity.this, ScoreActivity.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/moez_/maps/ScoreActivity.java
@@ -0,0 +1,21 @@
+package com.example.moez_.maps;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class ScoreActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_score);
+    }
+
+    public void returning (View view) {
+        // Let the user stop and return
+        Intent intent = new Intent(ScoreActivity.this, MainActivity.class);
+        startActivity(intent);
+    }
+}
